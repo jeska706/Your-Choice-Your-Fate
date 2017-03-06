a30cc1188703b6e0a396c831b020bc8ccdcf3689
jeska706 <jessicabeardsley@hotmail.com>
2017-03-06 09:50:53 -0700

    Set up user info

--- app.js
+++ app.js
@@ -1,10 +1,57 @@
 console.log('app.js is connected, choose wisely!');
 
 window.onload = function(){
+    //game init
     alert("You are about to embark on a grand adventure! \n Your fate is in your hands, choose wisely!");
+    //user's name
     var name = prompt("What is your name adventurer?");
-
-    console.log(name);
-
+    //welcome response
     alert("Welcome " + name + "!");
+
+    //starting hitpoints between 2-12
+    var startingHitPoints = Math.floor(Math.random()* 11 )+ 2;
+
+
+    //damage
+    var damage = startingHitPoints / 3;
+
+    alert("You have " + startingHitPoints + " hitpoints");
+
+    alert("You can do " + damage + " damage!");
+
+    var weapons = ['Sword', 'Roll of Tums', 'Shotgun', 'Candybar', 'Hot Coffee', 'Machine Gun', 'Knife'];
+    var weaponsDisplay = weapons.join(", ");
+    alert("These are your weapons: " + weaponsDisplay);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
